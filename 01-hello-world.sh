84262fc777fa5401b7ec48de7f6acb847abd5183
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 11:32:27 +0530

    Did some-Changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -41,9 +41,14 @@ read -s PASSWORD
 
 echo "Username is: $USERNAME, and password is: $PASSWORD"
 
+# Array's 06
+MOVIES=("RRR" "DjTillu" "murari") # size of above array is 3.
+# index are 0,    1,        2
 
-
-
+# list always starts with 0.
+echo "First Movie is: ${MOVIES[0]}"
+echo "Second Movie is :${MOVIES[1]}"
+echo "Third Movie is :${MOVIES[2]}"
 
 
 
