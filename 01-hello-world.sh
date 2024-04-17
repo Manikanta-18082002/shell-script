aca349689615d3545e5fd5434cb3e6b24926ab3a
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 08:16:18 +0530

    some-msg

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,58 @@
 #!/bin/bash
 
 echo "Iam learning Devops with AWS"
+
+# Simple Conversation
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh: I am fine too. how is your work?"
+echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
+# Draw back repeated words
+# Solution?  use variable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
