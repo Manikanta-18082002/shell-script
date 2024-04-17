e87b664786cd2e24b7fda2d17cbb8c88b2ec2766
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 12:02:02 +0530

    Did some-Changes

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -4,7 +4,7 @@
 echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script Name: $0"
-ehco "Current working Directory: $PWD"
+echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running the script: $USER"
 echo "Hostname: $HOSTNAME"
