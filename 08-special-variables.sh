d0bd647d2beb2e73a34d3520ed604e16ac504cbd
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 12:06:24 +0530

    Did some-Changes

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -8,4 +8,32 @@ echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running the script: $USER"
 echo "Hostname: $HOSTNAME"
-ehco "Process ID of current shell script $$"
+echo "Process ID of current shell script $$"
+sleep 60 
+echo "Process ID went to background command: $!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
