0d69476809522b0464b124c585e08305a96f1821
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 08:37:57 +0530

    Added Code

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,7 @@
 
 echo "Iam learning Devops with AWS"
 echo
-# Simple Conversation
+# Simple Conversation 02
 echo "Mani:: Hello Suresh, How are you?"
 echo "Jagan:: Hi Mani, I am fine. How are you?"
 echo "Mani: I am fine too. how is your work?"
@@ -11,7 +11,7 @@ echo "Jagan:: not bad. I am thinking to upgrade to DevOps"
 # Solution?  use variable
 echo
 
-#declaring of varible
+#declaring of varible 03
 PERSON1=Surya #no space between = and value
 PERSON2=Mani
 
@@ -21,7 +21,15 @@ echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
 
+#declaring of varible 04
+PERSON1=$1
+PERSON2=$2
 
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
 
 
 
