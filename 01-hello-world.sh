3e0995fe067e5e4f3b6631827e14aa3ce4122933
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-17 08:34:10 +0530

    Added Code

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Iam learning Devops with AWS"
-
+echo
 # Simple Conversation
 echo "Mani:: Hello Suresh, How are you?"
 echo "Jagan:: Hi Mani, I am fine. How are you?"
@@ -9,11 +9,12 @@ echo "Mani: I am fine too. how is your work?"
 echo "Jagan:: not bad. I am thinking to upgrade to DevOps"
 # Draw back repeated words
 # Solution?  use variable
-
+echo
 #declaring of varible
 PERSON1=Sachin #no space between = and value
 PERSON2=Rahul
 
+echo
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
