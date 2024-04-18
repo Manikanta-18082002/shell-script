a918097c40236674697b1e3b34cee366fd1eb3a9
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-18 11:08:01 +0530

    some-message

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -4,5 +4,31 @@ num1=$1
 num2=$2
 
 SUM=$((num1+num2))
+echo "Summation of $num1 + $num2 is: $SUM"
 
-echo "Sum of $num1 + $num2 is: $SUM"
+SUM=$((num1-num2))
+echo "Difference of $num1 - $num2 is: $SUM"
+
+SUM=$((num1/num2))
+echo "Division of $num1 / $num2 is: $SUM"
+
+SUM=$((num1*num2))
+echo "Multiply of $num1 * $num2 is: $SUM"
+
+SUM=$((num1%num2))
+echo "Modulo of $num1 % $num2 is: $SUM"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
