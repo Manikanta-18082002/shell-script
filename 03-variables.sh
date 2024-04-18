4f8d754ca80d68299bd7c41ca23d36abbcbcd6ac
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-18 10:30:04 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+P1=manoj
+P2=kanta
+
+echo "$P1:: Hello $P2, How are you?"
+echo "$P2:: Hi $1, I am fine. How are you?"
+echo "$P1: I am fine too. how is your work?"
+echo "$P2:: not bad. I am thinking to upgrade to DevOps"
