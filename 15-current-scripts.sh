a62dd7db3f84e35073b9f0d3b9198cd89aa37e49
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-23 16:18:40 +0530

    Doing changes

---
+++ 15-current-scripts.sh
@@ -0,0 +1,33 @@
+# How do you call other scripts from your shell script
+#(IMP Interview Q)
+
+#2 Ways
+
+#!/bin/bash
+ COURSE="DevOps from Current Script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+ 
+ ./16  -other-script.sh
+
+echo "After calling other script, course: $COURSE"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
