0a1ce8de45a1297bc7fbf907f52fd172c86e2135
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-23 16:33:19 +0530

    Doing changes

--- 15-current-scripts.sh
+++ 15-current-scripts.sh
@@ -9,7 +9,7 @@
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
  
- ./16-other-scripts.sh
+ # ./16-other-scripts.sh
 # Variable values will not change after calling other scripts
 #PID of 2 scripts are different
 
