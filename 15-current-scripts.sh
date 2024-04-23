0d065f2ecd1939094fb4a2f219089d54ac02439a
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-23 16:32:11 +0530

    Doing changes

--- 15-current-scripts.sh
+++ 15-current-scripts.sh
@@ -1,7 +1,7 @@
 # How do you call other scripts from your shell script
 #(IMP Interview Q)
 
-#2 Ways
+#2 Ways (./<script-Name>.sh  & source ./<script-Name>.sh)
 
 #!/bin/bash
  COURSE="DevOps from Current Script"
@@ -10,6 +10,12 @@ echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
  
  ./16-other-scripts.sh
+# Variable values will not change after calling other scripts
+#PID of 2 scripts are different
+
+source ./16-other-scripts.sh
+
+
 
 echo "After calling other script, course: $COURSE"
 
